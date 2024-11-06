d728cb51cd67df3556201732e3a021eedbf680b2
paulo.guerra <paulo.guerra@flex-am.com.br>
2024-11-06 13:55:14 -0400

    feat: get profile

---
+++ src/http/routes/get-profile.ts
@@ -0,0 +1,36 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { getUser } from '../../functions/get-user'
+import z from 'zod'
+import { authenticateUserHook } from '../hooks/authenticate-user'
+
+export const getProfileRoute: FastifyPluginAsyncZod = async app => {
+  app.get(
+    '/profile',
+    {
+      onRequest: [authenticateUserHook],
+      schema: {
+        tags: ['auth'],
+        description: 'Get authenticated user profile',
+        response: {
+          200: z.object({
+            profile: z.object({
+              id: z.string(),
+              name: z.string().nullable(),
+              email: z.string().nullable(),
+              avatarUrl: z.string().url(),
+            }),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const userId = request.user.sub
+
+      const { user } = await getUser({
+        userId,
+      })
+
+      return reply.status(200).send({ profile: user })
+    }
+  )
+}
